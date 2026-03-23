eb4644797158afedba1cff5eb0c67d65c8138def
sduhubert <husyl23@student.sdu.dk>
2026-03-23 22:21:55 +0100

    Add landing page with initial ui

--- sport-calendar/frontend/src/app/app.component.ts
+++ sport-calendar/frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopbarComponent } from './topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`
+  imports: [RouterOutlet, TopbarComponent],
+  template: `
+    <app-topbar />
+    <router-outlet />
+  `
 })
 export class AppComponent {}
